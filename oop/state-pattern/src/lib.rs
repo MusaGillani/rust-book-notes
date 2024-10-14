6fb4868e797c84b4d47d45ac49cb57a119327499
Musa Gillani <musagillaniwork@gmail.com>
2024-10-14 16:42:24 +0500

    oop
    state pattern re-structured to better use rust's type system

--- oop/state-pattern/src/lib.rs
+++ oop/state-pattern/src/lib.rs
@@ -86,3 +86,53 @@ impl State for Published {
         &post.content
     }
 }
+
+// encoding states and behaviours as types
+pub mod typed_state_pattern {
+    pub struct Post {
+        content: String,
+    }
+
+    pub struct DraftPost {
+        content: String,
+    }
+
+    impl Post {
+        pub fn new() -> DraftPost {
+            DraftPost {
+                content: String::new(),
+            }
+        }
+
+        pub fn content(&self) -> &str {
+            &self.content
+        }
+    }
+
+    impl DraftPost {
+        pub fn add_text(&mut self, text: &str) {
+            self.content.push_str(text);
+        }
+
+        // takes ownership of self
+        // hence consuming DraftPost when PendingReviewPost is created
+        // (DraftPost not in scope after this function hence droppped from mem)
+        pub fn request_review(self) -> PendingReviewPost {
+            PendingReviewPost {
+                content: self.content,
+            }
+        }
+    }
+
+    pub struct PendingReviewPost {
+        content: String,
+    }
+
+    impl PendingReviewPost {
+        pub fn approve(self) -> Post {
+            Post {
+                content: self.content,
+            }
+        }
+    }
+}
--- oop/state-pattern/src/main.rs
+++ oop/state-pattern/src/main.rs
@@ -1,6 +1,7 @@
-use state_pattern::Post;
+use state_pattern::{typed_state_pattern::Post as TPost, Post};
 
 fn main() {
+    println!("State Objects approach");
     let mut post = Post::new();
 
     post.add_text("I ate a salad for lunch today");
@@ -11,4 +12,15 @@ fn main() {
 
     post.approve();
     assert_eq!("I ate a salad for lunch today", post.content());
+
+    println!("Typed State approach");
+    let mut typed_post = TPost::new();
+
+    typed_post.add_text("I ate a salad for lunch today");
+
+    let typed_post = typed_post.request_review();
+
+    let typed_post = typed_post.approve();
+
+    assert_eq!("I ate a salad for lunch today", typed_post.content());
 }
